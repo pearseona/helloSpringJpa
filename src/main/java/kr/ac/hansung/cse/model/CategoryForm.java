2cdfc9a97c678443d1f33b9b14bcb7ac03a5803e
Baeseona <baeseona2930@gmail.com>
2026-04-15 20:43:01 +0900

    feat: Category 목록 및 등록 화면(VIew) 및 DTO 구현 완료

--- src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -1,4 +1,22 @@
 package kr.ac.hansung.cse.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name;
+
+    public CategoryForm(String name) {
+        this.name = name;
+    }
 }
+
+
